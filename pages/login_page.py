9df45ac0c84604f5a7b3e52dfa3157cf39097791
DenisGnilamedov <77381857+DenisGnilamedov@users.noreply.github.com>
2021-12-12 17:31:10 +0300

    added locators.py, login_page.py and 3 methods for login_page.py

---
+++ pages/login_page.py
@@ -0,0 +1,24 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert "login" in self.browser.current_url, 'This is not a login page!'
+
+    def should_be_login_form(self):
+        self.browser.find_element(*LoginPageLocators.LOGIN_EMAIL), 'Login E-mail is not presented'
+        self.browser.find_element(*LoginPageLocators.LOGIN_PASSWORD), 'Login password is not presented'
+        assert True
+
+    def should_be_register_form(self):
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_EMAIL), 'Registration E-mail is not presented'
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD), 'Registration password is not presented'
+        self.browser.find_element(*LoginPageLocators.REGISTRATION_PASSWORD_REPEAT), 'Registration password repeat ' \
+                                                                                    'is not presented'
+        assert True
